dceb8f18db478e64164df0ca73dfdfbe7da8fea0
sd <sd@atechlab.eu>
2019-08-09 17:27:31 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func ReadDirCustom(dirname string, printFiles bool) ([]os.FileInfo, error) {
+	var dirsOnly []os.FileInfo
+	files, err := ioutil.ReadDir(dirname)
+	if !printFiles {
+		for _, f := range files {
+			if f.IsDir() {
+				dirsOnly = append(dirsOnly, f)
+			}
+		}
+		return dirsOnly, err
+	}
+	return files, err
+}
+
+/*
+func printFile(prefix string, name string, printFiles, size int64) {
+}
+*/
+
+func dirTreeRecur(out io.Writer, path string, printFiles bool, prefix string) error {
+	files, err := ReadDirCustom(path, printFiles)
+	for index, f := range files {
+		if index == len(files)-1 {
+			if f.IsDir() {
+				fmt.Println(prefix + "└───" + f.Name())
+				fmt.Printf("%v", f.Size())
+				dirTreeRecur(out, path+string(os.PathSeparator)+f.Name(), printFiles, prefix+"    ")
+			} else {
+				//fmt.Printf(prefix+"├───"+f.Name(), "("+string(f.Size())+")\n")
+				fmt.Printf("%v└───%v (%vb)\n", prefix, f.Name(), f.Size())
+			}
+		} else {
+			if f.IsDir() {
+				fmt.Println(prefix + "├───" + f.Name())
+				dirTreeRecur(out, path+string(os.PathSeparator)+f.Name(), printFiles, prefix+"│   ")
+			} else {
+				fmt.Printf("%v├───%v (%vb)\n", prefix, f.Name(), f.Size())
+			}
+		}
+
+	}
+	return err
+}
+func dirTree(out io.Writer, path string, printFiles bool) error {
+	//fmt.Println(path)
+	return dirTreeRecur(out, path, printFiles, "")
+}
+
+func main() {
+	out := os.Stdout
+	path := "."
+	if len(os.Args) == 2 || len(os.Args) == 3 {
+		//panic("usage go run main.go . [-f]")
+		path = os.Args[1]
+	}
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
